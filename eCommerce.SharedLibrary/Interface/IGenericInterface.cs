c239a661813d3570d3202a9f7c185b40cdef2f6d
Bojan Mirkovic <bojanmirkovic81@gmail.com>
2025-01-20 14:52:39 +0100

    ok

--- eCommerce.SharedLibrary/Interface/IGenericInterface.cs
+++ eCommerce.SharedLibrary/Interface/IGenericInterface.cs
@@ -8,7 +8,7 @@ namespace eCommerce.SharedLibrary.Interface
         Task<Response> CreateAsync(T entity);
         Task<Response> UpadteAsync(T entity);
         Task<Response> DeleteAsync(T entity);
-        Task<IEnumerable<T>> GetaAllAsync();
+        Task<IEnumerable<T>> GetAllAsync();
         Task<T?>FindByIdAsync(int id);
         Task<T?> GetByAsync(Expression<Func<T, bool>> predicate);
     }
